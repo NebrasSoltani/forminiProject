a95713ffdec2bc53619d5019ef3576cbb60edc14
NebrasSoltani <nebras.soltani@esprit.tn>
2026-02-06 13:00:49 +0100

    template

---
+++ src/Repository/ApprenantRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Apprenant;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Apprenant>
+ */
+class ApprenantRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Apprenant::class);
+    }
+}
