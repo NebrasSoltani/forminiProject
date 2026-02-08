7eef3d6870b77782e28f0291b36b4ab39454b799
NebrasSoltani <nebras.soltani@esprit.tn>
2026-02-08 01:59:05 +0100

    crud domaines

---
+++ src/Enum/Gouvernorat.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Enum;
+
+enum Gouvernorat: string
+{
+    case TUNIS = 'Tunis';
+    case ARIANA = 'Ariana';
+    case BEN_AROUS = 'Ben Arous';
+    case MANOUBA = 'Manouba';
+    case NABEUL = 'Nabeul';
+    case SOUSSE = 'Sousse';
+    case SFAX = 'Sfax';
+    case KAIROUAN = 'Kairouan';
+    case BIZERTE = 'Bizerte';
+    case GABES = 'Gabès';
+    case MEDENINE = 'Médenine';
+    case TATAOUINE = 'Tataouine';
+    case TOZEUR = 'Tozeur';
+    case KEBILI = 'Kébili';
+    case BEJA = 'Béja';
+    case JENDOUBA = 'Jendouba';
+    case KEF = 'Le Kef';
+    case SILIANA = 'Siliana';
+    case MAHDIA = 'Mahdia';
+    case MONASTIR = 'Monastir';
+    case ZAGHOUAN = 'Zaghouan';
+}
